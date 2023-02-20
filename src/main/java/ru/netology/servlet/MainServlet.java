d6341c8511b42df70294931f8de759b4701b73d4
Roman <61121402+rebrov-rc@users.noreply.github.com>
2023-02-20 19:44:11 +0300

    di java created

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
@@ -20,16 +22,16 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+    final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+    final var repository = context.getBean(PostRepository.class);
+    final var service = context.getBean(PostService.class);
+    controller = context.getBean(PostController.class);
   }
 
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) {
 
 
-    // если деплоились в root context, то достаточно этого
     try {
       final var path = req.getRequestURI();
       final var method = req.getMethod();
